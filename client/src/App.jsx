9dd75b0f4bdeba41b76f3fb8fa10ec3c735de3bb
Vlad <vlad_glivuk@gmail.com>
2021-09-05 14:40:16 +0300

    init client with base layout

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <main className="main">
+      <div className="main__container">
+        <div className="main__form">
+          <input type="text" className="main__inp" placeholder="url"/>
+          <button className="main__btn">Shorten</button>
+        </div>       
+      </div>
+    </main>
+  );
+}
+
+export default App;
