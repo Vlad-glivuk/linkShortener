706556745880bc7401af43b2e8443edab6b2b4c3
Vlad <vlad_glivuk@gmail.com>
2021-09-05 19:09:26 +0300

    client: update App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
+import axios  from 'axios';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({ inp: "", shortenedLink: "" })
+  function handleInput (e) {
+    setState({...state, inp: e.target.value})
+    
+  }
+  function handleClick () {
+    console.log(state)
+    axios.post("http://localhost:3000/shorten-link", {fullLink: state.inp
+  }).then(res => setState({...state, shortenedLink: res.data})).catch(e => console.log(e))
+  }
 
   return (
     <main className="main">
       <div className="main__container">
         <div className="main__form">
-          <input type="text" className="main__inp" placeholder="url"/>
-          <button className="main__btn">Shorten</button>
-        </div>       
+          <input type="text" className="main__inp" placeholder="url" onInput={handleInput}/>
+          <button className="main__btn" onClick={handleClick}>Shorten</button>
+          {state.shortenedLink && <span className="main__res" >shortened link: {state.shortenedLink}</span>}
+        </div>
       </div>
     </main>
   );
